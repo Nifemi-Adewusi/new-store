95ce2cf92c599d259a42c100bd03c56bba4b4d9f
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-21 18:25:54 +0100

    Update order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -96,7 +96,7 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
 
-          <Button disabled={isSubmitting}>
+          <Button type="primary" disabled={isSubmitting}>
             {isSubmitting ? "Placing Orders..." : "Order Now"}
           </Button>
         </div>
