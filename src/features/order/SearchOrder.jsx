402ae6efc0992d8cc62e0b69af3a541dbbd04949
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-21 18:26:42 +0100

    Update searchorder

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+
 function SearchOrder() {
   const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
