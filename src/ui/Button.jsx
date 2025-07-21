a9afb28a4837eaf20c1a4c7b864fb0baf8181788
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-21 18:28:10 +0100

    Update the Button Styles

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
-function Button({ disabled, children }) {
+function Button({ disabled, children, type }) {
   return (
     <button
-      className="bg-yellow-400 font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-8 md:py-4"
+      className={`${type === "small" ? "text-xs px-4 py-3" : ""}bg-yellow-400 font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-8 md:py-4`}
       disabled={disabled}
     >
       {children}
