e2ebc0af008b119e5f378fcd94ad2d646b031af0
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-21 18:25:06 +0100

    Update MenuItem

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -2,21 +2,35 @@
 /* eslint-disable react/prop-types */
 
 import { formatCurrency } from "../../utils/helpers";
-function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+ import Button from "../../ui/Button";
+ function MenuItem({ pizza }) {
+   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
-  return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
-        </div>
-      </div>
-    </li>
-  );
-}
+   return (
+     <li className="sm:flex gap-4 py-2">
+       <img
+         className={`h-24 ${soldOut ? "opacity-50 grayscale" : ""}`}
+         src={imageUrl}
+         alt={name}
+       />
+       <div className="flex flex-col grow">
+         <p className="font-medium">{name}</p>
+         <p className="text-sm italic text-stone-500 capitalize">
+           {ingredients.join(", ")}
+         </p>
+         <div className="md:mt-auto mt-2 flex items-center justify-between">
+           {!soldOut ? (
+             <p className="text-sm">{formatCurrency(unitPrice)}</p>
+           ) : (
+             <p className="text-sm font-medium uppercase text-stone-500">
+               Sold out
+             </p>
+           )}
+           <Button type="small">Add To Cart</Button>
+         </div>
+       </div>
+     </li>
+   );
+ }
 
 export default MenuItem;
