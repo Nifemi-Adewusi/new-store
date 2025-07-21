d1f164267f779b18df6c67980887a89e5f3a934e
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-21 18:24:19 +0100

    Update Menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -7,12 +7,10 @@ function Menu() {
   const menu = useLoaderData();
   console.log(menu);
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => {
         // <MenuItem key={pizza.id} pizza={pizza} />;
-        return (
-          <MenuItem key = {pizza.id} pizza = {pizza}/>
-        )
+        return <MenuItem key={pizza.id} pizza={pizza} />;
       })}
     </ul>
   );
