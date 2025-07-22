68779dcb98e57ed9111739d81a8c01f41407419b
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-22 12:30:06 +0100

    Update Features

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,13 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import Button from "../../ui/Button";
+import { formatCurrency } from "../../utils/helpers";
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li>
-      <p>
+    <li className="py-3 sm:flex sm:items-center sm:justify-center">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
+      <div className="flex items-center sm:gap-6 justify-between grow">
+        <p className="text-sm font-bold sm:mt-1 sm:ml-4">
+          {formatCurrency(totalPrice)}
+        </p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
