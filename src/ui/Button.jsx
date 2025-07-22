aca31777c121f6f3c4bd6d493f3b8ebdcfe85d55
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-22 12:31:18 +0100

    Update Button Features

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,8 +1,21 @@
 /* eslint-disable react/prop-types */
-function Button({ disabled, children, type }) {
+import {useNavigate} from 'react-router-dom'
+function Button({ disabled, children, type, to }) {
+  const navigate = useNavigate();
+  if (to) {
+    return (
+      <button
+        onClick={() => navigate(to)}
+        className={`${type === "small" ? "text-xs px-4 py-3" : ""} bg-yellow-400 font-semibold text-sm text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-8 md:py-4`}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    );
+  }
   return (
     <button
-      className={`${type === "small" ? "text-xs px-4 py-3" : ""}bg-yellow-400 font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-8 md:py-4`}
+      className={`${type === "small" ? "text-xs px-4 py-3" : ""} bg-yellow-400 font-semibold text-sm text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-8 md:py-4`}
       disabled={disabled}
     >
       {children}
