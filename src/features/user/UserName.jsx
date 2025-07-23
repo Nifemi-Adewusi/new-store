23661d43c3f6422dae3a4a3ee9a3dc4431025862
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-23 16:14:51 +0100

    Update userName

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux";
+import { getGreeting } from "../../utils/helpers";
 function UserName() {
-    return (
-        <div className="text-sm font-semibold md:text-xl">
-            Nifemi
-        </div>
-    )
+  const userName = useSelector((state) => state.user.userName);
+  if (!userName) return;
+  return (
+    <div className="text-xs font-semibold md:text-lg">
+      {getGreeting()}, {userName}
+    </div>
+  );
 }
 
-export default UserName
+export default UserName;
