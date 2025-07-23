14e0f77dee97f9fa6a4652fca7eae755631d23f2
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-23 16:19:39 +0100

    Update Cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -4,6 +4,7 @@ import LinkButton from "../../ui/LinkButton";
 import Button, { ButtonSecondary } from "../../ui/Button";
 import CartItem from "./CartItem";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const fakeCart = [
   {
@@ -31,7 +32,7 @@ const fakeCart = [
 
 function Cart() {
   const cart = fakeCart;
-  const [newCart, setNewCart] = useState(fakeCart);
+  const [newCart, setNewCart] = useState(cart);
   const handleDelete = (pizzaId) => {
     const updatedCart = newCart.filter((item) => item.pizzaId !== pizzaId);
     setNewCart(updatedCart);
@@ -39,6 +40,8 @@ function Cart() {
   const handleClearCart = () => {
     setNewCart([]);
   };
+  const userName = useSelector((state) => state.user.userName);
+  console.log(userName);
   return (
     <div>
       {/* <Link
@@ -50,7 +53,7 @@ function Cart() {
 
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {userName}</h2>
       <ul className="divide-y divide-stone-200 border-b mt-3">
         {newCart.map((item) => (
           <CartItem
