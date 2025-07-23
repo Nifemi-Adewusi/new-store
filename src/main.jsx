7d90dac1b24577049b86e0471e52cb8ff3fb5b38
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-23 11:29:55 +0100

    Use Redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import New from "./Ride/HomePage.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store.js";
 // import Flow from "./Ride/Flow.jsx";
 
 // const context = createContext()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
     {/* <New /> */}
     {/* <Flow /> */}
   </React.StrictMode>
