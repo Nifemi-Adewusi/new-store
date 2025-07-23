3bba6cd2bc9562a7704583287e38c96a9ffb2b34
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-23 16:13:34 +0100

    Update AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,21 +2,22 @@ import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
 import Loader from './Loader';
+// import { getTotalCartQuantity } from "../features/cart/CartSlice";
 
 function AppLayout() {
   const navigation = useNavigation();
   console.log(navigation);
   const isLoading = navigation.state === "loading";
   return (
-    <div className = "grid grid-rows-[auto_1fr_auto] h-screen">
-      {isLoading && <Loader/>}
+    <div className="grid grid-rows-[auto_1fr_auto] h-screen">
+      {isLoading && <Loader />}
       <Header />
       {/* <div className="overflow-scroll"> */}
       <main className=" max-w-3xl mx-auto">
         <Outlet />
-        </main>
+      </main>
       {/* </div> */}
-        
+
       <CartOverview />
     </div>
   );
