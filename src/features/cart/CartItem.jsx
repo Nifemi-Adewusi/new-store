568dd834250c09328941837e8aa948dcee603a2b
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-24 10:17:00 +0100

    update cartItem

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -2,8 +2,15 @@
 /* eslint-disable react/prop-types */
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
-function CartItem({ item, handleDelete }) {
+import { useDispatch } from "react-redux";
+import { deleteItem } from "./CartSlice";
+function CartItem({ item }) {
+  const dispatch = useDispatch();
+
   const { pizzaId, name, quantity, totalPrice } = item;
+  const handleDelete = () => {
+    dispatch(deleteItem(pizzaId));
+  };
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-center">
       <p className="mb-1 sm:mb-0">
@@ -13,7 +20,7 @@ function CartItem({ item, handleDelete }) {
         <p className="text-sm font-bold sm:mt-1 sm:ml-4">
           {formatCurrency(totalPrice)}
         </p>
-        <Button click={() => handleDelete(pizzaId)} type="small">
+        <Button click={handleDelete} type="small">
           Delete
         </Button>
       </div>
