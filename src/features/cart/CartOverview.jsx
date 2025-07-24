9bad906064fce131f7d71e764160620017753e12
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-24 10:06:06 +0100

    Update CartOverview

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,14 +1,21 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-
+import { getTotalCartPrice, getTotalCartQuantity } from "./CartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const price = useSelector(getTotalCartPrice);
+  if (!totalCartQuantity) return null;
   return (
     <div className="bg-stone-800 px-4 py-4 text-stone-200 uppercase sm:px-6 flex justify-between items-center">
       <p className="text-stone-300 font-semibold gap-5 text-sm md:text-base flex space-x-4">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(price)}</span>
       </p>
-      <Link className="" to="/cart">Open cart &rarr;</Link>
+      <Link className="" to="/cart">
+        Open cart &rarr;
+      </Link>
     </div>
   );
 }
