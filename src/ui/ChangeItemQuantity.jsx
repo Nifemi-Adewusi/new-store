99b13c529d96b3711d492dfe3ce5eea7d219f2b4
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-29 04:21:14 +0100

    Update ChangeItem to reflect and include the span for better control

--- src/ui/ChangeItemQuantity.jsx
+++ src/ui/ChangeItemQuantity.jsx
@@ -3,24 +3,25 @@ import { useDispatch } from "react-redux"
 import Button from "./Button"
 import { decreaseItemQuantity, increaseItemQuantity } from "../features/cart/CartSlice"
 
-function ChangeItemQuantity({ pizzaId }) {
-    const dispatch = useDispatch()
-    return (
-      <div className="flex items-center gap-2">
-        <Button
-          type="round"
-          click={() => dispatch(increaseItemQuantity(pizzaId))}
-        >
-          +
-        </Button>
-        <Button
-          type="round"
-          click={() => dispatch(decreaseItemQuantity(pizzaId))}
-        >
-          -
-        </Button>
-      </div>
-    );
+function ChangeItemQuantity({ pizzaId, currentQuantity }) {
+  const dispatch = useDispatch();
+  return (
+    <div className="flex items-center gap-2">
+      <Button
+        type="round"
+        click={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +
+      </Button>
+      <span>{currentQuantity}</span>
+      <Button
+        type="round"
+        click={() => dispatch(decreaseItemQuantity(pizzaId))}
+      >
+        -
+      </Button>
+    </div>
+  );
 }
 
 
