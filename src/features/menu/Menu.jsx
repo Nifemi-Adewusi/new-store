315f12c03d47b8ed946d325c259f12e8a179a603
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-29 04:18:22 +0100

    Update Menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -5,7 +5,7 @@ import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
 function Menu() {
   const menu = useLoaderData();
-  // console.log(menu);
+  console.log(menu);
 
   return (
     <ul className="divide-y divide-stone-200 px-2">
