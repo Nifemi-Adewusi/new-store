efffac5e4b65fa56cd3ba6a56cb77be169663d91
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-31 04:12:17 +0100

    Updated version

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,10 +5,12 @@ import UserName from "../features/user/UserName";
 function Header() {
   return (
     <header className="bg-yellow-500 uppercase md:text-2xl  px-4 py-4 border-b  border-stone-200 gap-3 items-center flex flex-col sm:px-6 sm:flex-row sm:justify-around">
-      <Link className="tracking-widest" to="/">Fast React Pizza Company</Link>
+      <Link className="tracking-widest" to="/">
+        Pizza Company
+      </Link>
       <SearchOrder />
       {/* <p>Nifemi</p> */}
-      <UserName/>
+      <UserName />
     </header>
   );
 }
